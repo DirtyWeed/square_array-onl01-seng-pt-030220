e3a545d6cd63537321ca215dcf0d2952350e4443
Dylan Williamson <dirtyassweed@icloud.com>
2020-03-09 00:21:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,12 @@
+def square_array(numbers)
+  new =[]
+  numbers.each { |i| new << i ** 2}
+
 def square_array(array)
-    array.each do |i|
-      i ** 2
-      puts i
-    end
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
+
+my_arr = [1, 2]
+p square_array(my_arr)
